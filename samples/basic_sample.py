17940d90a60760e277baccf99ed32be2e256f1a2
Meritxell Vinyals <meritxell.vinyals@inrae.fr>
2022-11-09 16:14:51 +0100

    Add local variables file to specify user local variables

--- samples/basic_sample.py
+++ samples/basic_sample.py
@@ -1,9 +1,11 @@
 import gym
 import gama_gym #TODO: think if we can get rid of this import in someway
+import user_local_variables as lv
+
 #Create the environment
 env = gym.make('GamaEnv-v0',
-             headless_directory      = r"/home/mvinyalssal/Documents/GAMA_1.8.2_Linux_with_JDK_11.08.22_a02b4025/headless",
-             headless_script_path    = r"/home/mvinyalssal/Documents/GAMA_1.8.2_Linux_with_JDK_11.08.22_a02b4025/headless/gama-headless.sh",    
+             headless_directory      = lv.headless_dir,
+             headless_script_path    = lv.run_headless_script_path,    
              gaml_experiment_path    = "TCP_model_env_rc2.gaml",
              gaml_experiment_name    = "one_simulation",
              gama_server_url         = "localhost",
---
+++ samples/user_local_variables.py
@@ -0,0 +1,2 @@
+headless_dir = # Path to the directory of gama headless folder                                                               
+run_headless_script_path    = # Path to the script that runs gama headless (gama-headless.sh)
