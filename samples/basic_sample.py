8dfdce51a9250d242e947ef9586444dd5b20172e
Meritxell Vinyals <meritxell.vinyals@inrae.fr>
2022-11-10 08:31:23 +0100

    commit changes made home

--- samples/basic_sample.py
+++ samples/basic_sample.py
@@ -6,7 +6,17 @@ import user_local_variables as lv
 env = gym.make('GamaEnv-v0',
              headless_directory      = lv.headless_dir,
              headless_script_path    = lv.run_headless_script_path,    
-             gaml_experiment_path    = "TCP_model_env_rc2.gaml",
+             gaml_experiment_path    = r"/home/mvinyalssal/code/gama-gym/samples/TCP_model_env_rc2.gaml",
              gaml_experiment_name    = "one_simulation",
              gama_server_url         = "localhost",
              gama_server_port        = 6868)
+
+done = False
+initial_observation = env.reset()
+print('initial_observation ', initial_observation)
+while not done:
+    action = np.random.rand(1,5).flatten()
+    next_observation, reward, done, info = env.step(action)
+    print('done', done)
+    done = False
+
