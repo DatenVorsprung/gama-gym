b4c9a99e92e4457bcacfdef623c668ece75250dd
Meritxell Vinyals <meritxell.vinyals@inrae.fr>
2022-11-21 12:04:41 +0100

    Modify basic_sample with proper using and adds gamamain_vanilla_samply tyring to reproduce other project error

--- samples/basic_sample.py
+++ samples/basic_sample.py
@@ -5,20 +5,25 @@ import os
 import numpy as np
 
 CURRENT_DIRECTORY = os.path.abspath(os.getcwd())
-#Create the environment
-env = gym.make('GamaEnv-v0',
+
+try:
+    #Create the environment
+    env = gym.make('GamaEnv-v0',
              headless_directory      = lv.headless_dir,
              headless_script_path    = lv.run_headless_script_path,    
              gaml_experiment_path    = CURRENT_DIRECTORY+r"/TCP_model_env_rc2.gaml",
              gaml_experiment_name    = "one_simulation",
              gama_server_url         = "localhost",
              gama_server_port        = 6868)
-
-done = False
-initial_observation = env.reset()
-print('initial_observation ', initial_observation)
-while not done:
-    action = np.random.rand(1,5).flatten()
-    next_observation, reward, done, info = env.step(action)
-    print('done ', done)
-
+    # Test that run 2 complete episodes with the same environment
+    n_iters = 2
+    for iter in range(n_iters):
+        initial_observation = env.reset()
+        print('initial_observation ', initial_observation)
+        done = False
+        while not done:
+            action = np.random.rand(1,5).flatten()
+            next_observation, reward, done, info = env.step(action)
+            print('done ', done)
+finally:
+    env.close()
